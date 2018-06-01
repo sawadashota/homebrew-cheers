56841102cf9b1a5b2a6eea213a28734726c3b027
sawadashota <xiootas@gmail.com>
2018-06-01 17:36:57 +0900

    Brew formula update for gocmd version v1.0.11

--- gocmd.rb
+++ gocmd.rb
@@ -1,15 +1,15 @@
 class Gocmd < Formula
   desc "Golang cli management tool"
   homepage ""
-  url "https://github.com/sawadashota/waitopen/releases/download/v0.0.2/waitopen_0.0.2_macOS_64-bit.tar.gz"
-  version "0.0.2"
-  sha256 "eff607eceee088939ba30d76d98450943a90675e064f4ff502963eb7f4dae8e5"
+  url "https://github.com/sawadashota/gocmd/releases/download/v1.0.11/gocmd_1.0.11_macOS_64-bit.tar.gz"
+  version "1.0.11"
+  sha256 "35028953920a9b91407222c1b6e40a98a04bb1b6e8ca53d43866bb567d40f1f0"
 
   def install
-    bin.install "wait-open"
+    bin.install "gocmd"
   end
 
   test do
-    system "#{bin}/wait-open -h"
+    system "#{bin}/gocmd -h"
   end
 end
