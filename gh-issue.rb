7b129c6dfb22af7427471df4aea667cb2cbda1dd
sawadashota <xiootas@gmail.com>
2018-09-27 12:05:59 +0900

    Brew formula update for gh-issue version v0.4.1

--- gh-issue.rb
+++ gh-issue.rb
@@ -1,9 +1,9 @@
 class GhIssue < Formula
   desc "Create GitHub issues from YAML"
   homepage ""
-  url "https://github.com/sawadashota/gh-issue/releases/download/v0.4.0/gh-issue_0.4.0_macOS_64-bit.tar.gz"
-  version "0.4.0"
-  sha256 "63a0ac24a75acd7e65ffa2049692905002798da83fd465fb5e8615f75703f028"
+  url "https://github.com/sawadashota/gh-issue/releases/download/v0.4.1/gh-issue_0.4.1_macOS_64-bit.tar.gz"
+  version "0.4.1"
+  sha256 "f178ac41c9e532cd597ede94ead00a19b7a758ef1a430cfafcb94f9721dc628d"
   
   depends_on "envchain"
 
