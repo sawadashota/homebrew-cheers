d1dcac7ea583efeb87ca949aec22e45237cceb76
sawadashota <xiootas@gmail.com>
2018-12-06 23:47:44 +0900

    Brew formula update for gocmd version v1.0.18

--- gocmd.rb
+++ gocmd.rb
@@ -1,9 +1,9 @@
 class Gocmd < Formula
   desc "Golang cli management tool"
   homepage ""
-  url "https://github.com/sawadashota/gocmd/releases/download/v1.0.17/gocmd_1.0.17_macOS_64-bit.tar.gz"
-  version "1.0.17"
-  sha256 "8bbe2c0f72d0c2a8a29a9e2ba0f7a80bc18ca690d09e266d99e69596489af2de"
+  url "https://github.com/sawadashota/gocmd/releases/download/v1.0.18/gocmd_1.0.18_macOS_64-bit.tar.gz"
+  version "1.0.18"
+  sha256 "c9ed6801efca34f1335bdc72a2399b88f85e5c8bdb10e1d0beeea27c83a0a730"
 
   def install
     bin.install "gocmd"
