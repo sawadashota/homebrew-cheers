e4186b00f4b2891c350ea431373e04d55bfb0ec0
sawadashota <xiootas@gmail.com>
2019-12-16 21:52:46 +0900

    Brew formula update for orb-update version 0.1.6

--- orb-update.rb
+++ orb-update.rb
@@ -2,16 +2,16 @@
 class OrbUpdate < Formula
   desc "Update Orbs versions"
   homepage ""
-  version "0.1.5"
+  version "0.1.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sawadashota/orb-update/releases/download/0.1.5/orb-update_0.1.5_macOS_64-bit.tar.gz"
-    sha256 "6a8c376cec73c47f2b2a8277ce14cff8642250cf014b26e06ce7a10fbbb7aa44"
+    url "https://github.com/sawadashota/orb-update/releases/download/0.1.6/orb-update_0.1.6_macOS_64-bit.tar.gz"
+    sha256 "4d0393cecef0c086a1a81468aca209f9e7cddb9ed076a9b2faaa0bff63d9a1b5"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sawadashota/orb-update/releases/download/0.1.5/orb-update_0.1.5_linux_64-bit.tar.gz"
-      sha256 "31fcebb43029ac6f8b96f6feeb270053f39c774c53818915b0b45b62a0c5914a"
+      url "https://github.com/sawadashota/orb-update/releases/download/0.1.6/orb-update_0.1.6_linux_64-bit.tar.gz"
+      sha256 "169a70b0ed08a0bb3a936a029ac2f46dcaabc14f15647439a8def93756d4e23e"
     end
   end
 
